6be375b4924c376661ac1bbc0adf04fb9868e69d
gresm <78505251+gresm@users.noreply.github.com>
2023-08-22 14:31:02 +0200

    Create nova_docs.h

---
+++ src/nova_docs.h
@@ -0,0 +1,3 @@
+#define VECTOR_OBJECT_X "X component of the vector"
+#define VECTOR_OBJECT_Y "Y component of the vector"
+#define SPACE_OBJECT_GRAVITY "Global and uniform gravity applied to all bodies in the space. For gravitational attraction between body pairs, see attractive bodies."
